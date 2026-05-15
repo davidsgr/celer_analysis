2e2c8b93918a99e8a999663b71385cddc5fce1c9
Gregory Davidson <davidsongg@ornl.gov>
2026-05-15 08:22:13 -0400

    Initial commit of analysis worksheet and post-processing script

---
+++ dune/process_results.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import json
+import csv
+
+# Define number of runs
+num_runs = 10
+
+# Prepare data for writing CSV file
+fieldnames = ["run", "absorption", "along-step", "local-vacancies", "optical-boundary-init", 
+              "optical-boundary-post", "optical-discrete-select", "optical-rayleigh", 
+              "optical-surface-stepping", "pre-step", "primary-generate", "tracking-cut",
+              "setup", "total"]
+fielddata = list()
+    
+# Extract the timing data from all of the runs
+for i in range(num_runs):
+    
+    # Open the JSON file for run i
+    input_filename = "run_{}/out.{}.json".format(i, i)
+    with open(input_filename, "r", encoding="utf-8") as input:
+
+        # Read and parse the JSON file
+        json_input = json.load(input)
+
+        # Load the timings into a dictionary
+        data = dict();
+        data["run"] = str(i)
+            
+        # Get the action timings
+        for fn in fieldnames[1,-2]:
+            print(fn)
+            data[fn] = json_input['result']['time']['actions'][fn]
+            
+        # Get the summary timings
+        for fn in fieldnames[-2, -1]:
+            print(fn)
+            data[fn] = json['result']['time'][fn]
+            
+        # Append to the CSV field
+        fielddata.append(data)
+
+# Write the CSV file for the detailed timing information for all of the runs
+with open("detailed_timings.csv", "w", newline="\n", encoding="utf-8") as output:
+    writer = csv.DictWriter(output, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows(fielddata)
+
+# Process the field data to get timing averages over all of the runs
+avg_data = dict()
+for fn in fieldnames:
+    
+    # Construct a list of field fn over all runs
+    list_data = [fielddata[i][fn] for i in range(num_runs)]
+    
+    # Compute the average and store in the dictionary
+    avg_data[fn] = sum(list_data) / num_runs
+
+# Write the CSV file for the average timing information
+with open("average_timings.csv", "w", newline="\n", encoding="utf-8") as output:
+    writer = csv.DictWriter(output, fieldnames=fieldnames)
+    writer.writeheader()
+    writer.writerows([avg_data])
