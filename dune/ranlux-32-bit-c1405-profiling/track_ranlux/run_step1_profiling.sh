08757234b04927641351e24a9a73dce30d5824c8
Gregory Davidson <davidsongg@ornl.gov>
2026-07-15 09:50:30 -0400

    Remove old results

--- dune/ranlux-32-bit-c1405-profiling/track_ranlux/run_step1_profiling.sh
+++
@@ -1,7 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 CELER_ENABLE_PROFILING=1 nsys profile \
-    --trace=cuda,nvtx,osrt \
-    --osrt-backtrace-stack-size=16384 \
-    --backtrace=fp \
-    -o dune-zwires-case1 \
-    -f true \
-    /scratch/gqe/install/make-ranlux-32-bit-c1405-profiling/bin/celer-optical run-gpu.json
--- dune/ranlux-32-bit-c1405-profiling/track_ranlux/run_step2_profiling.sh
+++
@@ -1,2 +0,0 @@
-CUDA_VISIBLE_DEVICES=1 TMPDIR=$HOME/tmp/ncu ncu --set full --kernel-name launch_action_impl --replay-mode application --launch-skip 261 --launch-count 1 -o dune-rayleigh-profiling \
-/scratch/gqe/install/make-ranlux-32-bit-c1405-profiling/bin/celer-optical run-gpu.json
